9767803478cfaab402eb6426db682aeb14af6f29
Jeremy Mason <jmason@informatics.jax.org>
2011-01-11 13:09:22 -0500

    updates to formatting and template

--- main.py
+++ main.py
@@ -1,46 +1,84 @@
+"""
+Twitter backup for Google App Engine.
+
+Use this app to archive a twitter stream to GAE.  The twitter API currently
+only archives up to 3200 historical tweets(!). Use this to go back further
+than that.
+"""
+
+# -- Imports -------------------------------------------------------------
 import datetime
 import cgi
 import os
 
 import simplejson
+
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-# == models =======================================================
+
+# -- Models --------------------------------------------------------------
 class Tweet(db.Model):
+    """One twitter entry"""
+
     content = db.StringProperty(required = True)
-    owner = db.UserProperty(required = True)
     date = db.DateTimeProperty(auto_now_add=True)
-
-
-class TwitterConfig(db.Model):
-    content = db.StringProperty(required = True)
     owner = db.UserProperty(required = True)
-    date = db.DateTimeProperty(auto_now_add=True)
 
-# == controllers =================================================
+
+# -- Twitter db entries --------------------------------------------------
+# -- https://github.com/tav/tweetapp
+class OAuthRequestToken(db.Model):
+    """OAuth Request Token."""
+
+    service = db.StringProperty()
+    oauth_token = db.StringProperty()
+    oauth_token_secret = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    owner = db.UserProperty(required = True)
+
+
+class OAuthAccessToken(db.Model):
+    """OAuth Access Token."""
+
+    service = db.StringProperty()
+    specifier = db.StringProperty()
+    oauth_token = db.StringProperty()
+    oauth_token_secret = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    owner = db.UserProperty(required = True)
+
+
+# -- Controllers ---------------------------------------------------------
 class Welcome(webapp.RequestHandler):
+    """The welcome page, information and login"""
 
     def get(self):
+
         if users.get_current_user():
             self.redirect("/tweets", False)
-        login_url = users.create_login_url(self.request.uri)
-        self.response.out.write(template.render('welcome.html', {'login_url':login_url}))
+
+        kwargs = {
+            'login_url': users.create_login_url(self.request.uri)
+            }
+
+        self.response.out.write(template.render('welcome.html', kwargs))
 
 
 class Tweets(webapp.RequestHandler):
+    """Paginated collection of tweets"""
 
     def get(self):
+
         if not users.get_current_user():
             self.redirect("/")
 
-        q = Tweets.all().order('date')
-        tweets = q.fetch(10)
+        tweets = Tweets.all().order('date').fetch(10)
 
-        template_values = {
+        kwargs = {
             'tweets': tweets,
             'user': users.get_current_user(),
             'url': users.create_logout_url(self.request.uri),
@@ -49,40 +87,56 @@ class Tweets(webapp.RequestHandler):
             'year': datetime.datetime.now().year
             }
 
-        self.response.out.write(template.render('index.html', template_values))
+        self.response.out.write(template.render('index.html', kwargs))
 
-    def post(self):
-        tweet = Tweet(content = self.request.get('content'),
+    def refresh(self):
+
+        tweet = Tweet(
+            content = self.request.get('content'),
             owner = users.get_current_user(),
             )
+
         tweet.put()
+
         self.redirect('/tweets')
 
+
 class Configure(webapp.RequestHandler):
+    """Configure which twitter account to archive"""
     
     def get(self):
+
         if not users.get_current_user():
             self.redirect("/")
 
-        template_values = {
+        kwargs = {
             'user': users.get_current_user(),
             'url': users.create_logout_url(self.request.uri),
             'url_linktext': 'Logout',
             'request': self.request,
             'year': datetime.datetime.now().year
             }
-            #TODO: create configure page
-        self.response.out.write(template.render('configure.html', template_values))
 
-application = webapp.WSGIApplication(
-                                     [
-                                     ('/', Welcome),
-                                     ('/tweets', Tweets)
-                                     ],
-                                     debug=True)
+        #TODO: create configure html page
+
+        self.response.out.write(
+            template.render('configure.html', kwargs))
+
+# -- The main GAE application and routes ---------------------------------
+application = webapp.WSGIApplication([
+    ('/', Welcome),
+    ('/tweets', Tweets),
+    ('/refresh', Tweets.refresh),
+    ('/configure', Configure),
+    ], debug=True)
+
+
+# -- The main function is cached on GAE ----------------------------------
 def main():
     run_wsgi_app(application)
 
+
+# -- DO IT! --------------------------------------------------------------
 if __name__ == "__main__":
     main()
 
